d7d4b41f27c06403461d0d74731831b353a78662
David Silva <davidslv@gmail.com>
2020-04-02 18:56:48 +0100

    Initial Commit

---
+++ SlotsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsDemo
+//
+//  Created by David Silva on 02/04/2020.
+//  Copyright © 2020 David Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
